5487bc6e6a65bd5a5c8bc0b9f1585b8be0f81e1d
Lakshay <nijhawanlakshay06@gmail.com>
2024-03-23 19:30:37 +0530

    second commit

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -19,52 +19,52 @@ function ProjectsPage() {
           My Projects
       </h1>
       <div className="flex flex-wrap justify-center">
-            {projectsData.projects.map((project) => (
-                <CardContainer className="inter-var">
-                <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+      {projectsData.projects.map((project) => (
+            <CardContainer key={project.id} className="inter-var">
+              <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
+                <CardItem
+                  translateZ="50"
+                  className="text-xl font-bold text-neutral-600 dark:text-white"
+                >
+                  {project.title}
+                </CardItem>
+                <CardItem
+                  as="p"
+                  translateZ="60"
+                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                >
+                  {project.description}
+                </CardItem>
+                <CardItem translateZ="100" className="w-full mt-4">
+                  <Image
+                    src={project.image}
+                    height="1000"
+                    width="1000"
+                    className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                    alt={project.title}
+                  />
+                </CardItem>
+                <div className="flex justify-between items-center mt-10">
                   <CardItem
-                    translateZ="50"
-                    className="text-xl font-bold text-neutral-600 dark:text-white"
+                    translateZ={20}
+                    as="button"
+                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
                   >
-                    {project.title}
+                    Check project →
                   </CardItem>
                   <CardItem
-                    as="p"
-                    translateZ="60"
-                    className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                    translateZ={20}
+                    as="button"
+                    className="px-4 py-1 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
                   >
-                    {project.description}
+                    <Link href={project.link} target="_blank">
+                      Visit
+                    </Link>
                   </CardItem>
-                  <CardItem translateZ="100" className="w-full mt-4">
-                    <Image
-                      src={project.image}
-                      height="1000"
-                      width="1000"
-                      className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-                      alt={project.title}
-                      />
-                  </CardItem>
-                  <div className="flex justify-between items-center mt-10">
-                    <CardItem
-                      translateZ={20}
-                      as="button"
-                      className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-                      >
-                      Check project →
-                    </CardItem>
-                    <CardItem
-                      translateZ={20}
-                      as="button"
-                      className="px-4 py-1 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
-                      >
-                      <Link href={project.link} target="_blank">
-                        Visit
-                      </Link>
-                    </CardItem>
-                  </div>
-                </CardBody>
-              </CardContainer>
-            ))}
+                </div>
+              </CardBody>
+            </CardContainer>
+          ))}
         </div> 
       </div>
       </TracingBeam>
